cee9694e257554613af24d016b9ab5e9e12072be
Paul H <161055444+flopsllc@users.noreply.github.com>
2024-06-12 17:08:44 -0400

    got everything working again

--- src/Integration.php
+++ src/Integration.php
@@ -9,16 +9,11 @@ Util\class_loader('Settings');
 
 function install( Util\Plugin $plugin ) {
 
-	if ( current_user_can( 'manage_options' ) ) {
-		if ( $plugin->get_setting( key: 'import_url' ) ) {
-			add_filter( 'page_row_actions', __NAMESPACE__ . '\add_sync_link', 10, 2 );
-		}
-	}
+	add_global_settings( $plugin );
 
-	add_global_settings_page( $plugin );
 }
 
-function add_global_settings_page( Util\Plugin $plugin ) {
+function add_global_settings( Util\Plugin $plugin ) {
 
 	$plugin->add_new_settings(
 		menu_title: __( 'Content Sync', $plugin->i18n_domain ),
@@ -92,30 +87,6 @@ function add_global_settings_page( Util\Plugin $plugin ) {
 		</script>
 		<?php
 	} );
-}
-
-/**
- * Adds a 'sync' link to the list of post row actions if the post type is allowed.
- * Implemented as a filter of page_row_actions
- *
- * @param array $actions The list of post row actions.
- * @param \WP_Post $post The post object.
- * @return array The modified list of post row actions.
- */
-function add_sync_link( $actions, $post = null ) {
-	if ( ! $post ) {
-		return $actions;
-	}
-	if ( ! in_array( $post->post_type, allowed_post_types() ) ) {
-		return $actions;
-	}
-	$actions['sync'] = sprintf(
-		'<a href="%s" data-content-sync="%d">%s</a>',
-		admin_url( 'admin.php?page=content-sync-post&post_id=' . $post->ID ),
-		$post->ID,
-		__( 'Sync', 'content-sync' )
-	);
-	return $actions;
 }
 
 function allowed_post_types() {
